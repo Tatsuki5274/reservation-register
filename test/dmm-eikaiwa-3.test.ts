b2b212507f4528326e4f46c87567b308c379ef5e
Tatsuki <private.1.5810@fastmail.jp>
2022-03-27 05:54:58 +0900

    Initial commit

---
+++ test/dmm-eikaiwa-3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DmmEikaiwa3 from '../lib/dmm-eikaiwa-3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dmm-eikaiwa-3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DmmEikaiwa3.DmmEikaiwa3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
