1d49b142d1276170028338c62ee6c6c955c372f9
Tatsuki <private.1.5810@fastmail.jp>
2022-03-27 10:48:15 +0900

    downgrade node version to 14

--- lambda/mailChecker/index.ts
+++ lambda/mailChecker/index.ts
@@ -1,15 +1,27 @@
 import * as AWS from "aws-sdk";
 import { env } from "process";
+import * as Imap from "imap";
+// const Imap = require("imap");
+import * as Util from "util";
+const inspect = Util.inspect;
 
-var client = new AWS.SecretsManager({
-  region: "ap-northeast-1",
+var imap = new Imap({
+  user: "mygmailname@gmail.com",
+  password: "mygmailpassword",
+  host: "imap.gmail.com",
+  port: 993,
+  tls: true,
 });
-const secretName = env.SECRET_NAME;
-if (!secretName) {
-  throw new Error("Secret name is not defined");
-}
 
-export const handler = async () => {
+const getImapConnection = async () => {
+  var client = new AWS.SecretsManager({
+    region: "ap-northeast-1",
+  });
+  const secretName = env.SECRET_NAME;
+  if (!secretName) {
+    throw new Error("Secret name is not defined");
+  }
+
   const secret = await await client
     .getSecretValue({ SecretId: secretName })
     .promise();
@@ -17,9 +29,30 @@ export const handler = async () => {
     throw new Error("SecretString is empty");
   }
   const secretObject = JSON.parse(secret.SecretString);
-  const userName: unknown = secretObject.USER_NAME;
+
+  const userName: unknown = secretObject.IMAP_USER_NAME;
   if (typeof userName !== "string") {
-    throw new Error("USER_NAME is not string");
+    throw new Error("IMAP_USER_NAME is not string");
   }
-  return userName;
+  const password: unknown = secretObject.IMAP_PASSWORD;
+  if (typeof password !== "string") {
+    throw new Error("IMAP_PASSWORD is not string");
+  }
+  const host: unknown = secretObject.IMAP_HOST;
+  if (typeof host !== "string") {
+    throw new Error("IMAP_HOST is not string");
+  }
+  // const imap = new Imap({
+  //   user: userName,
+  //   password: password,
+  //   host: host,
+  //   port: 993,
+  //   tls: true,
+  // });
+  // return imap;
+};
+
+export const handler = async () => {
+  const imap = await getImapConnection();
+  return "success!";
 };
